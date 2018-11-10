42f81ecd19aa9ffde88cfc0b344d528a9b3ddb3c
Emma Savoret <emmasavoret@hotmail.co.uk>
2018-11-10 11:26:11 +0000

    Add endpoints for fetching and deleting items

--- index.ts
+++ index.ts
@@ -18,5 +18,17 @@ app.get('/api/todos/new', (request: express.Request, response: express.Response)
     response.send(todos);
 });
 
+app.get('/api/todos/fetch', (request: express.Request, response: express.Response) => {
+    const position = request.query.position;
+    const result = todos[position];
+    response.send(result);
+});
+
+app.get('/api/todos/delete', (request: express.Request, response: express.Response) => {
+    const entry = request.query.entry;
+    todos.splice(entry,1);
+    response.send(todos);
+});
+
 app.listen(3000); // spinning up a server
 console.log('Listening on port 3000');
