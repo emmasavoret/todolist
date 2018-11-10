12ccb9d43ab5b9b6a45a495ba93d11ba6ea24796
Emma Savoret <emmasavoret@hotmail.co.uk>
2018-11-10 13:18:10 +0000

    Make Endpoints more RESTful

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { isNumber } from 'util';
 
 const app = express();
 
@@ -6,7 +7,7 @@ app.get('/api', (request: express.Request, response: express.Response) => {
     response.send('hello'); 
 });
 
-const todos: string[] = [];
+const todos: string[] = ['a', 'b', 'c', 'd'];
 
 app.get('/api/todos', (request: express.Request, response: express.Response) => {
     response.send(todos);
@@ -18,16 +19,38 @@ app.get('/api/todos/new', (request: express.Request, response: express.Response)
     response.send(todos);
 });
 
-app.get('/api/todos/fetch', (request: express.Request, response: express.Response) => {
-    const position = request.query.position;
+app.get('/api/todos/:position', (request: express.Request, response: express.Response) => {
+    const position = request.params.position;
+
+    // guard pattern
+    if (!isNumber(position)) {
+        response.status(400).send('Not a valid position');
+        return;
+    }
+
+    if (position < 0) {
+        response.status(400).send('Not a valid position');
+        return;
+    }
+
+    // because it is protecting the rest of that function against a bad input
     const result = todos[position];
     response.send(result);
 });
 
-app.get('/api/todos/delete', (request: express.Request, response: express.Response) => {
-    const entry = request.query.entry;
-    todos.splice(entry,1);
-    response.send(todos);
+app.delete('/api/todos/:entry', (request: express.Request, response: express.Response) => {
+    const entry = request.params.entry;
+    const parsed = parseInt(entry, 10);
+    if (isNaN(parsed)) {
+        response.status(400).send('Not a valid position');
+    } else {
+        if (parsed < 0) {
+            response.status(400).send('Not a valid position');
+        } else {
+            todos.splice(entry,1);
+            response.send(todos);   
+        }
+    }
 });
 
 app.listen(3000); // spinning up a server
